bdfa9eef45955120d8f6f99a35330b1414d753e5
0marSalah <omar.salah1597@gmail.com>
2023-08-26 23:43:55 +0300

    fuck lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
-# execute a command -- kill process -- only if a condition is true
+# execute a command only if a condition is true
 
 exec { 'kill-killmenow-process':
-  command   => 'pkill killmenow',
-  onlyif    => 'pgrep killmenow',
-  path      => '/usr/bin:/usr/sbin:/bin',
+  command => 'pkill killmenow',
+  onlyif  => 'pgrep killmenow',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
